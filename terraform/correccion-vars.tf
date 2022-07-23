4bada4d0d1e76f68bcc03b1b5d8fd2399ccd2311
Rebeca Delgado <rebekadc@gmail.com>
2022-07-23 14:09:15 +0100

    version funcional

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -1,3 +1,29 @@
+variable "location" {
+  type = string
+  description = "Región de Azure donde crearemos la infraestructura"
+  default = "uksouth" 
+}
+
+variable "storage_account" {
+  type = string
+  description = "Nombre para la storage account"
+  default = "cp2unir072022"
+}
+
+variable "public_key_path" {
+  type = string
+  description = "Ruta para la clave pública de acceso a las instancias"
+  default = "~/.ssh/id_rsa.pub" 
+}
+
+
+variable "ssh_user" {
+  type = string
+  description = "Usuario para hacer ssh"
+  default = "azureuser"
+}
+
+# ---- variables utilizadas en la practica ----
 variable "resource_group_name" {
   default = "rg-createdbyTF"
 }
@@ -13,15 +39,20 @@ variable "network_name" {
 variable "subnet_name" {
   default = "subnet1"
 }
-
+# Los nombres de las maquinas virtuales se estableceran segun la posición del array "vms" de forma que
+# en la posicion 0 se definira el nombre para la maquina master, en la posicion 1 el nombre para la maquina
+# worker y en la posicion 2 en nombre para la maquina nfs
 variable "vms" {
   description = "Maquinas virtuales"
   type        = list(string)
   default     = ["master", "worker", "nfs"]
 }
-
+# Los tamaños de las maquinas virtuales se estableceran segun la posición del array "sizes" de forma que
+# en la posicion 0 se definira el tipo de la maquina master, en la posicion 1 el el tipo de la maquina
+# worker y en la posicion 2 el tipo de la maquina nfs
 variable "sizes" {
   description = "Tipos de las maquinas virtuales"
   type        = list(string)
   default     = ["Standard_D2_v2", "Standard_A2_v2", "Standard_A2_v2"]
+  
 }
